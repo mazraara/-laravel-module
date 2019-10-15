522a054c91ba2e8b307b8a8f20d0decae09ef48c
Azraar <azraar@gapstars.net>
2019-10-15 10:50:41 +0530

    npm install laravel-mix-merge-manifest --save-dev prevent the main Laravel Mix configuration from overwriting the

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
+
+/* Allow multiple Laravel Mix applications*/
+require('laravel-mix-merge-manifest');
+mix.mergeManifest();
+/*----------------------------------------*/
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
